6168581db3ccb4119a4083ff89203850d3e771bd
Cesar <cesar.mendoza.77@gmail.com>
2024-01-15 15:44:08 -0300

    prueba123

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,12 @@
-import React, { useState, useEffect } from 'react';
+// frontend/src/App.js
+import React from 'react';
 
 function App() {
-    const [message, setMessage] = useState('');
-
-    useEffect(() => {
-        // Hacer una solicitud a la API Flask
-        fetch('/api/hello')
-            .then(response => response.json())
-            .then(data => setMessage(data.message))
-            .catch(error => console.error('Error:', error));
-    }, []);
-
-    return (
-        <div className="App">
-            <h1>Reactsss App</h1>
-            <p>{message}</p>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Hello from React!</h1>
+    </div>
+  );
 }
 
 export default App;
