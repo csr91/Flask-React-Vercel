870ac8d3cbc8178c601ebe6ada699e855c1be7c5
Cesar <cesar.mendoza.77@gmail.com>
2024-01-16 14:36:31 -0300

    fetchreact

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 // frontend/src/App.js
-import React from 'react';
+import React, { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/api');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        console.log(data.message); // Muestra el mensaje en la consola
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []); // El segundo argumento [] asegura que el efecto se ejecute solo una vez al montar el componente
+
   return (
     <div>
       <h1>Hello from React!</h1>
