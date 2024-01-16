d3be8701f3f0ca04260787c98ad6cb87a4813f50
Cesar <cesar.mendoza.77@gmail.com>
2024-01-16 12:20:07 -0300

    pruebaba20

--- api/index.py
+++ api/index.py
@@ -2,14 +2,10 @@ from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 
-@app.route('/rct')
-def index():
-    return render_template('index.html')
-
 @app.route('/api')
 def hello():
-    return jsonify(message="Hell from Flask!")
+    return jsonify(message="Hell from Flaskkkkk!")
 
-@app.route('/apa')
+@app.route('/api/hello')
 def hellu():
-    return jsonify(message="Hell from Flask!")
+    return jsonify(message="Hello from Flask from api helllooo!!!")
